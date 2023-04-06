bc388e6ff7d1f37ba2ac1612252bd46af750f3a3
scibaric <sebastijan.cibaric@gmail.com>
2023-04-06 22:09:56 +0200

    Set endpoint entry as singular, will revert it later

--- src/main/java/dev/scibaric/meterreadings/controller/MeterController.java
+++ src/main/java/dev/scibaric/meterreadings/controller/MeterController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/meters")
+@RequestMapping("/api/meter")
 public class MeterController {
 
     private final MeterService service;
